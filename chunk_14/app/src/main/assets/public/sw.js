c2b7c13d60449f1f35fe30cca9c623c3c47380e1
DavidTheProfessionalBusinessManGuyDude <davidmrowe2711@gmail.com>
2026-02-20 19:42:05 -0500

    Add files via upload

---
+++ chunk_14/app/src/main/assets/public/sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "sciencelift-v2";
+const APP_ASSETS = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./icons/icon-180.png",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) return caches.delete(key);
+                    return null;
+                })
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cached) => cached || fetch(event.request))
+    );
+});
---
+++ chunk_14/app/src/main/java/com/sciencelift/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sciencelift.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
